2262dcb1449df4c0835a163f68f321c49d0268f2
Antonio Pierro <antonio.pierro@gmail.com>
2018-01-22 13:30:08 +0100

    Added dove-siamo.php page

---
+++ dove-siamo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require 'head.php';?>
+</head>
+<body>
+    <header>
+        <img src="img/header-bg.png">
+    </header>
+    <?php require 'navigation.php';?>
+
+    <section class="page-section clearfix">
+        <div class="container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3091.0172397888787!2d9.124721215359891!3d39.219774179522275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e734725d2dc121%3A0x7a215f3a81181664!2sVia+Giacomo+Puccini%2C+09128+Cagliari+CA!5e0!3m2!1sen!2sit!4v1516624142666" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </section>
+    <?php require 'footer.php';?>
+    <?php require 'js.php';?>
+</body>
+</html>
--- navigation.php
+++ navigation.php
@@ -20,6 +20,9 @@
         <li class="nav-item px-lg-4">
           <a class="nav-link text-uppercase text-expanded" href="#store.html">Store</a>
         </li>
+        <li class="nav-item px-lg-4">
+          <a class="nav-link text-uppercase text-expanded" href="dove-siamo.php">Dove siamo</a>
+        </li>
       </ul>
     </div>
   </div>
