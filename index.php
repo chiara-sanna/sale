385a277ba63fa72d3fad7a8e51d106ab32c42ae1
Antonio Pierro <antonio.pierro@gmail.com>
2018-01-22 13:23:53 +0100

    splitted the index.html into multiple php files

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require 'head.php';?>
+</head>
+<body>
+    <header>
+        <img src="img/header-bg.png">
+    </header>
+    <?php require 'navigation.php';?>
+
+    <section class="page-section clearfix">
+        <div class="container">
+            <div class="intro">
+                <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="img/intro.png" alt="">
+                <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+                    <h2 class="section-heading mb-4">
+                        <span class="section-heading-upper">Sanna Legno</span>
+                        <span class="section-heading-lower">Falegnameria Arredamenti</span>
+                    </h2>
+                    <p class="mb-3">Progettiamo e realizziamo bar e locali pubblici porte interne infissi esterni zanzariere e sistemi oscuranti mobili su misura e artigianali arredamenti completi classici e moderni!
+                    </p>
+                    <div class="intro-button mx-auto">
+                        <a class="btn btn-primary btn-xl" href="#">Visit Us Today!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="page-section cta">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-9 mx-auto">
+                    <div class="cta-inner text-center rounded">
+                        <h2 class="section-heading mb-4">
+                            <span class="section-heading-upper">Our Promise</span>
+                            <span class="section-heading-lower">To You</span>
+                        </h2>
+                        <p class="mb-0">Garantiamo professionalità e competenza dalla progettazione alla posa in opera!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="page-section">
+        <div class="container">
+            <div class="product-item">
+                <div class="product-item-title d-flex">
+                    <div class="bg-faded p-5 d-flex mr-auto rounded">
+                        <h2 class="section-heading mb-0">
+                            <span class="section-heading-upper">La tradizione che non muore</span>
+                            <span class="section-heading-lower">Foto di oggetti antichi</span>
+                        </h2>
+                    </div>
+                </div>
+                <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="img/oggetti-antichi.png" alt="">
+                <div class="product-item-description d-flex ml-auto">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php require 'footer.php';?>
+    <?php require 'js.php';?>
+</body>
+</html>
