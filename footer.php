d5a6a3441a1941189868ccc71a46e0f98761c29f
chiara-sanna <kiara.s90@tiscali.it>
2018-02-03 12:42:11 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
 <footer class="footer text-faded text-center py-5">
   <div class="container">
-    <p class="m-0 small">Copyright &copy; Sanna Legno Group srl - St. Prov.le per Cheremule, 07040 Cheremule (SS)</p>
+    <p class="m-0 upper">Copyright &copy; Sanna Legno Group srl - St. Prov.le per Cheremule, 07040 Cheremule (SS)</p>
     <div class="section">
          <span class="section-footer-medium">Tel. Fax 079-889007, cell. 339-2853877</span>
+           <span class="section-footer-lower">e-mail info@sannalegno.it P.Iva 02281780904</span>
       </div>
-    <div class="section">
-             <span class="section-footer-lower">e-mail info@sannalegno.it P.Iva 02281780904</span>
-  </div>
 </footer>
