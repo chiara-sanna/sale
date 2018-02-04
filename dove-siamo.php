beecc211a26cd9199bb447d178575882c3778745
chiara-sanna <kiara.s90@tiscali.it>
2018-02-04 21:37:28 +0100

    Update dove-siamo.php

--- dove-siamo.php
+++ dove-siamo.php
@@ -22,7 +22,8 @@
       </section>
     <script>
 
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3033.6860899293315!2d8.724021315117882!3d40.50432405823187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12dc45ca764e865f%3A0x11bba73a27315f59!2sSanna+Legno+Srl!5e0!3m2!1sen!2sit!4v1517776525485" 
+        width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
       function initMap() {
         var cheremule = {lat: 40.507728, lng: 8.726293};
