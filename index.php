0c30b61467ddbbf2c31b743e3f3d2ce46ce0cf4d
chiara-sanna <kiara.s90@tiscali.it>
2018-02-04 22:23:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <?php require 'head.php';?>
 </head>
 <body>
-    <iframe name="Dreaming your room with RoomStyler" src="https://roomstyler.com/" height="100" widht="200">
-    </iframe>
-    <li><iframe src=\"https://roomstyler.com/" width="188" height="258" scrolling="no" style="overflow:hidden; margin-top:-4px; margin-left:-4px; border:none;"></iframe></li>
+    <iframe name="Dreaming your room with RoomStyler" src="https://roomstyler.com/" height="100" widht="200"></iframe>
+    <iframe src=\"https://roomstyler.com/" width="188" height="258" scrolling="no"></iframe>
+    <iframe src="https://www.w3schools.com"></iframe>
+    
+    
     <header>
         <img src="img/header-bg.png">
     </header>
