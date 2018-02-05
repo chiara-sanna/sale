e5ff69965a01dda56e7037d001c00355db52e360
chiara-sanna <kiara.s90@tiscali.it>
2018-02-05 18:44:00 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
           </a>
            </div>
         <div class="section">
-          <span class="section-footer-lower"> <img src="img/follow-us.png" width="100"> </span>
+          <span class="section-footer-lower"> <img src="img/follow-us.png" width="200"> </span>
       </div>
          
 </footer>
