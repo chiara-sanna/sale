6b8103e869acbb0c543c56e7d9417d28704c67f7
chiara-sanna <kiara.s90@tiscali.it>
2018-02-05 22:17:09 +0100

    Update Contact-Us.php

--- Contact-Us.php
+++ Contact-Us.php
@@ -46,6 +46,32 @@
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBnCMGOQvSAnDwPA4MFuBtmRjJW0_DRuM&callback=initMap">
     </script>
+    
+<form action="/cgi-bin/FormMail.pl" method="POST">
+<input type=hidden name="recipient" value="valerio_fadda@live.it">
+<input type=hidden name="subject" value="Nuovo messaggio dal sito web">
+<table border="0" cellspacing="0" cellpadding="5" align="center">
+ <tr>
+ <td><b> Nome e Cognome:</b></td>
+ <td align="right"><input type=text name="realname"></td>
+ </tr>
+ <tr>
+ <td><b>Email:</b></td>
+ <td align="right"><input type=text name="email"></td>
+ </tr>
+ <tr>
+ <td colspan="2"><b>Messaggio:</b></td>
+ </tr>
+ <tr>
+ <td colspan="2"><textarea name="messaggio" cols="40" rows="5"></textarea></td>
+ </tr>
+ <tr>
+ <td colspan="2" align="center"><input type="Submit" value="Invia"></td>
+ </tr>
+</table>
+<input type=hidden name="required" value="email,realname,messaggio">
+</form>
+    
     <?php require 'footer.php';?>
     <?php require 'js.php';?>
 </body>
