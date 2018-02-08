8d1ce7eb824c80500ac634fcab6f779c4f4101ad
chiara-sanna <kiara.s90@tiscali.it>
2018-02-08 12:19:14 +0100

    Update stanza-virtuale.php

--- stanza-virtuale.php
+++ stanza-virtuale.php
@@ -18,7 +18,7 @@
     </header>
     <?php require 'navigation.php';?>
       <div class="container">
-        <iframe name="Dreaming your room with RoomStyler" src="https://planner.roomsketcher.com/?ctxt=rs_com" width="100%" height="100%"></iframe>
+        <iframe name="Dreaming your room with Living Spaces" src="https://www.livingspaces.com/inspiration/3d-room-designer" width="100%" height="100%"></iframe>
       </div>
     <?php require 'footer.php';?>
     <?php require 'js.php';?>
