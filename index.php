48b019be1ed9eb0f36694a9a3f5bd263242ca032
chiara-sanna <kiara.s90@tiscali.it>
2018-02-09 14:53:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
                                     <a class="btn btn-primary btn-xl" href="stanza-virtuale.php">Make your dream a home!</a>
                                 </div>
                             </div> 
-                            <img class="d-block w-75" src="img/intro.png" alt="Second slide">
+                            <img class="d-block w-75" src="img/studio-su-misura.jpg" alt="Second slide">
                         </div>
                         <div class="carousel-item">
                             <div class="intro-text left-0 text-center bg-faded p-5 rounded">
