b596567c3ebed2e2c87dd56b7fbb1a40569b6865
chiara-sanna <kiara.s90@tiscali.it>
2018-02-09 16:31:39 +0100

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -27,7 +27,7 @@
           
           <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          prodotti
+          PRODOTTI
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="antiquariato.php">Antiquariato</a>
@@ -43,10 +43,7 @@
         </li>
         <li class="nav-item px-lg-4">
           <a class="nav-link text-uppercase text-expanded" href="dove-siamo.php">Dove siamo</a>
-        </li>
-        <li class="nav-item px-lg-4">
-          <a class="nav-link text-uppercase text-expanded" href="prodotti.php">prodotti</a>
-        </li>
+          </li>
         <li class="nav-item px-lg-4">
           <a class="nav-link text-uppercase text-expanded" href="store.php">store</a>
         </li>
