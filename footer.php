99271411003bb835b00be7165a269b46a15be11d
chiara-sanna <kiara.s90@tiscali.it>
2018-02-09 17:02:05 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,8 +5,10 @@
          <span class="section-footer-medium">Tel. Fax 079-889007, cell. 339-2853877</span>
       <div class="section">
            <span class="section-footer-lower">e-mail info@sannalegno.it </span>
+         <div class="section">
+           <span class="section-footer-lower">pec: sannalegnogroupsrl@pec.it </span>
         <div class="section">
-          <span class="section-footer-lower">P.Iva 02281780904</span>
+          <span class="section-footer-lower">P.Iva 02725080903</span>
       </div>
         <div>
          <a href="https://www.facebook.com/sannalegno/"> <img src="img/facebook-icon.png" alt="Visita la pagina" width="50" >
