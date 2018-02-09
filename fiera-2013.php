68230b7c25ba9b0c321a7c57fdc7bcf4a8ad86dc
chiara-sanna <kiara.s90@tiscali.it>
2018-02-09 15:43:16 +0100

    Update fiera-2013.php

--- fiera-2013.php
+++ fiera-2013.php
@@ -30,7 +30,7 @@
                                 <h2 class="section-heading mb-4">
                                      <span class="section-heading-lower">Fiera 2013</span>
                                 </h2>
-                                <p class="mb-3">Progettiamo e realizziamo bar e locali pubblici porte interne, infissi esterni, zanzariere e sistemi oscuranti mobili su misura e artigianali arredamenti completi, classici e moderni!
+                                <p class="mb-3">Alcune immagini delle nostre esposizioni.
                                 </p>
                                 <div class="intro-button mx-auto">
                                     <a class="btn btn-primary btn-xl" href="stanza-virtuale.php">Make your dream a home!</a>
