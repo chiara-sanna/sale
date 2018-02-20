40eea874b63b50ffd1cd2b9508e041393bca358d
chiara-sanna <kiara.s90@tiscali.it>
2018-02-20 23:19:39 +0100

    Update dove-siamo.php

--- dove-siamo.php
+++ dove-siamo.php
@@ -24,7 +24,7 @@
         function initMap() {
         var cheremule = {lat: 40.507710, lng: 8.726339};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 20,
+          zoom: 18,
           center: cheremule,
           mapTypeId: google.maps.MapTypeId.HYBRID
         });
@@ -48,30 +48,7 @@
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBnCMGOQvSAnDwPA4MFuBtmRjJW0_DRuM&callback=initMap">
     </script>
     
-<form action="/cgi-bin/FormMail.pl" method="POST">
-<input type=hidden name="recipient" value="valerio_fadda@live.it">
-<input type=hidden name="subject" value="Nuovo messaggio dal sito web">
-<table border="0" cellspacing="0" cellpadding="5" align="center">
- <tr>
- <td><b> Nome e Cognome:</b></td>
- <td align="right"><input type=text name="realname"></td>
- </tr>
- <tr>
- <td><b>Email:</b></td>
- <td align="right"><input type=text name="email"></td>
- </tr>
- <tr>
- <td colspan="2"><b>Messaggio:</b></td>
- </tr>
- <tr>
- <td colspan="2"><textarea name="messaggio" cols="40" rows="5"></textarea></td>
- </tr>
- <tr>
- <td colspan="2" align="center"><input type="Submit" value="Invia"></td>
- </tr>
-</table>
-<input type=hidden name="required" value="email,realname,messaggio">
-</form>
+
     
     <?php require 'footer.php';?>
     <?php require 'js.php';?>
