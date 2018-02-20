5a8aecb1047410aede0fd8bf51d22bc661b7ee4a
chiara-sanna <kiara.s90@tiscali.it>
2018-02-20 23:34:32 +0100

    Update Contact-Us.php

--- Contact-Us.php
+++ Contact-Us.php
@@ -15,13 +15,13 @@
     <?php require 'navigation.php';?>
 
     <section class="page-section clearfix">
-        <div class="container style="height=600px">
+        <div class="container style="height:600px">
                
           </div>
 
       </section>
        
-<form action="/cgi-bin/FormMail.pl" method="POST">
+<form action="/cgi-bin/FormMail.pl" method="POST" style="margin-bottom:80px">
 <input type=hidden name="recipient" value="valerio_fadda@live.it">
 <input type=hidden name="subject" value="Nuovo messaggio dal sito web">
 <table border="0" cellspacing="0" cellpadding="5" align="center">
